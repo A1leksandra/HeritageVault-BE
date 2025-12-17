48dd8372466e0789b86ff5fcd6e079a125378103
A1leksandra <aleksandratokij@gmail.com>
2025-12-17 03:27:41 +0200

    missing migrations

---
+++ HV/HV.DAL/Migrations/20251217011511_AddRelations.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HV.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Landmarks_CityId",
+                table: "Landmarks",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_RegionId",
+                table: "Cities",
+                column: "RegionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_Countries_CountryId",
+                table: "Cities",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_Regions_RegionId",
+                table: "Cities",
+                column: "RegionId",
+                principalTable: "Regions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Landmarks_Cities_CityId",
+                table: "Landmarks",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Regions_Countries_CountryId",
+                table: "Regions",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_Countries_CountryId",
+                table: "Cities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_Regions_RegionId",
+                table: "Cities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Landmarks_Cities_CityId",
+                table: "Landmarks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Regions_Countries_CountryId",
+                table: "Regions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Landmarks_CityId",
+                table: "Landmarks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_RegionId",
+                table: "Cities");
+        }
+    }
+}
